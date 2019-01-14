470701faabb208fcc814ae9731af094fdd71340d
Alex Bar <haydruzibek@gmail.com>
2019-01-14 04:19:26 -0800

    word

---
+++ src/by/bsu/appAds/entity/Avtor.sql
@@ -0,0 +1,19 @@
+		 'id' number NOT NULL AUTO_INCREMENT,
+				 'name' VARCHAR(40) NOT NULL,
+				 PRIMARY KEY(id)
+				 'data' DATE,
+	) ENGINE
+	
+	create table 'compatition'(
+	'id' number not null auto_increment,
+	'muz_id' nvarchar2(50),
+	'kompozition_id' VARCHAR(50),
+	'mp3_id'
+	'muz_compoz_id'  VARCHAR(50),
+	)
+	CREATE TABLE 'music_type'(
+	'id' INTEGER NOT NULL AUTO_INCREMENT,
+	''
+	
+	)
+		  
